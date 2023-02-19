db778cd5a02fc2fbe94a43c4a54e1c0d1a295775
Gianmaria Bajo <mg1979.git@gmail.com>
2023-02-19 15:55:51 +0100

    fix: loop

--- lua/popup/util.lua
+++ lua/popup/util.lua
@@ -13,21 +13,19 @@ U.api = setmetatable({}, {
   end,
 })
 
-api = U.api
-
 --- Return true if a new scratch buffer was created for a popup.
 function U.is_temp_buffer(buf)
-  return api.buf_is_valid(buf or -1) and api.buf_get_var(buf, "popup_scratch_buffer")
+  return U.api.buf_is_valid(buf or -1) and U.api.buf_get_var(buf, "popup_scratch_buffer")
 end
 
 function U.delete_popup_buffers(p)
   if U.is_temp_buffer(p.buf) then
-    api.buf_delete(p.buf)
+    U.api.buf_delete(p.buf)
     local bbuf = p.buf - 1
     if p.wincfg.border and fn.bufexists(bbuf) == 1 and fn.buflisted(bbuf) == 0 then
-      local lines = api.buf_get_lines(bbuf, 1, -1, true)
+      local lines = U.api.buf_get_lines(bbuf, 1, -1, true)
       if #lines == 1 and lines[1] == '' then
-        api.buf_delete(bbuf)
+        U.api.buf_delete(bbuf)
       end
     end
   end
